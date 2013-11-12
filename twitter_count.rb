61f53f412a5b4e159b2a20658fb69e2a025c05fa
foreveryo <foreveryo@gmail.com>
2013-11-12 12:09:08 +0100

    The list comes from GDrive

--- twitter_count.rb
+++ twitter_count.rb
@@ -2,9 +2,21 @@
 
 require 'twitter'
 require './twitter_auth'
+require './GDrive'
 
-accounts = ["_cometeelmundo_", "equipatgedema"]
+# Connect to Google Drive and get the list of Twitter accounts
+drive = GDrive.new
+drive.Login
+ws = drive.openSheet("0AjPDjdc9aoO8dGNDNDZMci1Na0NJVHMyc2RXMm1DN1E",0)
+accounts = drive.readCol(ws,1,ws.num_rows)
 
+counter=0			#Initialize counter to 0
+accounts = accounts.uniq 	#Removing duplicates
+
+# Count the followers by account and save a counter for the total
 accounts.each do |username|
 	puts "#{username}: #{Twitter.user(username).followers_count}"
+	counter+=Twitter.user(username).followers_count
 end
+
+puts "Total followers #{counter}"
