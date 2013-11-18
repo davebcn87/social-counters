664d48a35ca4b13fbf19d70a93a59b90b3a33207
Aitor Andreu <foreveryo@gmail.com>
2013-11-18 09:18:56 +0100

    Update drive_auth.rb

--- drive_auth.rb
+++ drive_auth.rb
@@ -3,6 +3,6 @@ require 'google_drive'
 
 class DriveLogin
 	def getSession
-		return GoogleDrive.login("aitor.anvi@gmail.com", "torrada2559")
+		return GoogleDrive.login("aitor.anvi@gmail.com", "######")
 	end
 end
